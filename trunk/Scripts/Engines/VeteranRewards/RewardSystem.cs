e05312c7209e668b685fb197326dc67e836d9859
ShaiTanMalkier <ShaiTanMalkier@ab47cf70-d4a0-11dd-8179-53c65a4d6d95>
2009-02-28 01:19:48 +0000

    Changed how the Vet Reward Choice Gump acts.

--- trunk/Scripts/Engines/VeteranRewards/RewardSystem.cs
+++ trunk/Scripts/Engines/VeteranRewards/RewardSystem.cs
@@ -6,218 +6,218 @@ using Server.Accounting;
 
 namespace Server.Engines.VeteranRewards
 {
-	public class RewardSystem
-	{
-		private static RewardCategory[] m_Categories;
-		private static RewardList[] m_Lists;
+    public class RewardSystem
+    {
+        private static RewardCategory[] m_Categories;
+        private static RewardList[] m_Lists;
 
-		public static RewardCategory[] Categories
-		{
-			get
-			{
-				if ( m_Categories == null )
-					SetupRewardTables();
+        public static RewardCategory[] Categories
+        {
+            get
+            {
+                if (m_Categories == null)
+                    SetupRewardTables();
 
-				return m_Categories;
-			}
-		}
+                return m_Categories;
+            }
+        }
 
-		public static RewardList[] Lists
-		{
-			get
-			{
-				if ( m_Lists == null )
-					SetupRewardTables();
+        public static RewardList[] Lists
+        {
+            get
+            {
+                if (m_Lists == null)
+                    SetupRewardTables();
 
-				return m_Lists;
-			}
-		}
+                return m_Lists;
+            }
+        }
 
-		public static bool Enabled = true; // change to true to enable vet rewards
-		public static bool SkillCapRewards = true; // assuming vet rewards are enabled, should total skill cap bonuses be awarded? (720 skills total at 4th level)
-		public static TimeSpan RewardInterval = TimeSpan.FromDays( 30.0 );
+        public static bool Enabled = true; // change to true to enable vet rewards
+        public static bool SkillCapRewards = true; // assuming vet rewards are enabled, should total skill cap bonuses be awarded? (720 skills total at 4th level)
+        public static TimeSpan RewardInterval = TimeSpan.FromDays(30.0);
 
-		public static bool HasAccess( Mobile mob, RewardEntry entry )
-		{
-			TimeSpan ts;
-			return HasAccess( mob, entry.List, out ts );
-		}
+        public static bool HasAccess(Mobile mob, RewardEntry entry)
+        {
+            TimeSpan ts;
+            return HasAccess(mob, entry.List, out ts);
+        }
 
-		public static bool HasAccess( Mobile mob, RewardList list, out TimeSpan ts )
-		{
-			if ( list == null )
-			{
-				ts = TimeSpan.Zero;
-				return false;
-			}
+        public static bool HasAccess(Mobile mob, RewardList list, out TimeSpan ts)
+        {
+            if (list == null)
+            {
+                ts = TimeSpan.Zero;
+                return false;
+            }
 
-			Account acct = mob.Account as Account;
+            Account acct = mob.Account as Account;
 
-			if ( acct == null )
-			{
-				ts = TimeSpan.Zero;
-				return false;
-			}
+            if (acct == null)
+            {
+                ts = TimeSpan.Zero;
+                return false;
+            }
 
-			TimeSpan totalTime = (DateTime.Now - acct.Created);
+            TimeSpan totalTime = (DateTime.Now - acct.Created);
 
-			ts = ( list.Age - totalTime );
+            ts = (list.Age - totalTime);
 
-			if ( ts <= TimeSpan.Zero )
-				return true;
+            if (ts <= TimeSpan.Zero)
+                return true;
 
-			return false;
-		}
+            return false;
+        }
 
-		public static int GetRewardLevel( Mobile mob )
-		{
-			Account acct = mob.Account as Account;
+        public static int GetRewardLevel(Mobile mob)
+        {
+            Account acct = mob.Account as Account;
 
-			if ( acct == null )
-				return 0;
+            if (acct == null)
+                return 0;
 
-			return GetRewardLevel( acct );
-		}
+            return GetRewardLevel(acct);
+        }
 
-		public static int GetRewardLevel( Account acct )
-		{
-			TimeSpan totalTime = (DateTime.Now - acct.Created);
+        public static int GetRewardLevel(Account acct)
+        {
+            TimeSpan totalTime = (DateTime.Now - acct.Created);
 
-			int level = (int)(totalTime.TotalDays / RewardInterval.TotalDays);
+            int level = (int)(totalTime.TotalDays / RewardInterval.TotalDays);
 
-			if ( level < 0 )
-				level = 0;
+            if (level < 0)
+                level = 0;
 
-			return level;
-		}
+            return level;
+        }
 
-		public static bool ConsumeRewardPoint( Mobile mob )
-		{
-			int cur, max;
+        public static bool ConsumeRewardPoint(Mobile mob)
+        {
+            int cur, max;
 
-			ComputeRewardInfo( mob, out cur, out max );
+            ComputeRewardInfo(mob, out cur, out max);
 
-			if ( cur >= max )
-				return false;
+            if (cur >= max)
+                return false;
 
-			Account acct = mob.Account as Account;
+            Account acct = mob.Account as Account;
 
-			if ( acct == null )
-				return false;
+            if (acct == null)
+                return false;
 
-			//if ( mob.AccessLevel < AccessLevel.GameMaster )
-				acct.SetTag( "numRewardsChosen", (cur + 1).ToString() );
+            //if ( mob.AccessLevel < AccessLevel.GameMaster )
+            acct.SetTag("numRewardsChosen", (cur + 1).ToString());
 
-			return true;
-		}
+            return true;
+        }
 
-		public static void ComputeRewardInfo( Mobile mob, out int cur, out int max )
-		{
-			int level;
+        public static void ComputeRewardInfo(Mobile mob, out int cur, out int max)
+        {
+            int level;
 
-			ComputeRewardInfo( mob, out cur, out max, out level );
-		}
+            ComputeRewardInfo(mob, out cur, out max, out level);
+        }
 
-		public static void ComputeRewardInfo( Mobile mob, out int cur, out int max, out int level )
-		{
-			Account acct = mob.Account as Account;
+        public static void ComputeRewardInfo(Mobile mob, out int cur, out int max, out int level)
+        {
+            Account acct = mob.Account as Account;
 
-			if ( acct == null )
-			{
-				cur = max = level = 0;
-				return;
-			}
+            if (acct == null)
+            {
+                cur = max = level = 0;
+                return;
+            }
 
-			level = GetRewardLevel( acct );
+            level = GetRewardLevel(acct);
 
-			if ( level == 0 )
-			{
-				cur = max = 0;
-				return;
-			}
+            if (level == 0)
+            {
+                cur = max = 0;
+                return;
+            }
 
-			string tag = acct.GetTag( "numRewardsChosen" );
+            string tag = acct.GetTag("numRewardsChosen");
 
-			if ( String.IsNullOrEmpty( tag ) )
-				cur = 0;
-			else
-				cur = Utility.ToInt32( tag );
+            if (String.IsNullOrEmpty(tag))
+                cur = 0;
+            else
+                cur = Utility.ToInt32(tag);
 
-			if ( level >= 6 )
-				max = 9 + ((level - 6) * 2);
-			else
-				max = 2 + level;
-		}
+            if (level >= 6)
+                max = 9 + ((level - 6) * 2);
+            else
+                max = 2 + level;
+        }
 
-		public static bool CheckIsUsableBy( Mobile from, Item item, object[] args )
-		{
-			if ( m_Lists == null )
-				SetupRewardTables();
+        public static bool CheckIsUsableBy(Mobile from, Item item, object[] args)
+        {
+            if (m_Lists == null)
+                SetupRewardTables();
 
-			bool isRelaxedRules = ( item is DyeTub || item is MonsterStatuette );
+            bool isRelaxedRules = (item is DyeTub || item is MonsterStatuette);
 
-			Type type = item.GetType();
+            Type type = item.GetType();
 
-			for ( int i = 0; i < m_Lists.Length; ++i )
-			{
-				RewardList list = m_Lists[i];
-				RewardEntry[] entries = list.Entries;
-				TimeSpan ts;
+            for (int i = 0; i < m_Lists.Length; ++i)
+            {
+                RewardList list = m_Lists[i];
+                RewardEntry[] entries = list.Entries;
+                TimeSpan ts;
 
-				for ( int j = 0; j < entries.Length; ++j )
-				{
-					if ( entries[j].ItemType == type )
-					{
-						if ( args == null && entries[j].Args.Length == 0 )
-						{
-							if ( (!isRelaxedRules || i > 0) && !HasAccess( from, list, out ts ) )
-							{
-								from.SendLocalizedMessage( 1008126, true, Math.Ceiling( ts.TotalDays / 30.0 ).ToString() ); // Your account is not old enough to use this item. Months until you can use this item : 
-								return false;
-							}
+                for (int j = 0; j < entries.Length; ++j)
+                {
+                    if (entries[j].ItemType == type)
+                    {
+                        if (args == null && entries[j].Args.Length == 0)
+                        {
+                            if ((!isRelaxedRules || i > 0) && !HasAccess(from, list, out ts))
+                            {
+                                from.SendLocalizedMessage(1008126, true, Math.Ceiling(ts.TotalDays / 30.0).ToString()); // Your account is not old enough to use this item. Months until you can use this item : 
+                                return false;
+                            }
 
-							return true;
-						}
+                            return true;
+                        }
 
-						if ( args.Length == entries[j].Args.Length )
-						{
-							bool match = true;
+                        if (args.Length == entries[j].Args.Length)
+                        {
+                            bool match = true;
 
-							for ( int k = 0; match && k < args.Length; ++k )
-								match = ( args[k].Equals( entries[j].Args[k] ) );
+                            for (int k = 0; match && k < args.Length; ++k)
+                                match = (args[k].Equals(entries[j].Args[k]));
 
-							if ( match )
-							{
-								if ( (!isRelaxedRules || i > 0) && !HasAccess( from, list, out ts ) )
-								{
-									from.SendLocalizedMessage( 1008126, true, Math.Ceiling( ts.TotalDays / 30.0 ).ToString() ); // Your account is not old enough to use this item. Months until you can use this item : 
-									return false;
-								}
+                            if (match)
+                            {
+                                if ((!isRelaxedRules || i > 0) && !HasAccess(from, list, out ts))
+                                {
+                                    from.SendLocalizedMessage(1008126, true, Math.Ceiling(ts.TotalDays / 30.0).ToString()); // Your account is not old enough to use this item. Months until you can use this item : 
+                                    return false;
+                                }
 
-								return true;
-							}
-						}
-					}
-				}
-			}
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
 
-			// no entry?
-			return true;
-		}
+            // no entry?
+            return true;
+        }
 
-		public static void SetupRewardTables()
-		{
-			RewardCategory monsterStatues = new RewardCategory( 1049750 );
-			RewardCategory cloaksAndRobes = new RewardCategory( 1049752 );
-			RewardCategory etherealSteeds = new RewardCategory( 1049751 );
-			RewardCategory specialDyeTubs = new RewardCategory( 1049753 );
-			RewardCategory houseAddOns    = new RewardCategory( 1049754 );
-			
-			#region Veteran Rewards			
-			RewardCategory miscellaneous  = new RewardCategory( 1078596 );
-			#endregion
+        public static void SetupRewardTables()
+        {
+            RewardCategory monsterStatues = new RewardCategory(1049750);
+            RewardCategory cloaksAndRobes = new RewardCategory(1049752);
+            RewardCategory etherealSteeds = new RewardCategory(1049751);
+            RewardCategory specialDyeTubs = new RewardCategory(1049753);
+            RewardCategory houseAddOns = new RewardCategory(1049754);
 
-			m_Categories = new RewardCategory[]
+            #region Veteran Rewards
+            RewardCategory miscellaneous = new RewardCategory(1078596);
+            #endregion
+
+            m_Categories = new RewardCategory[]
 				{
 					monsterStatues,
 					cloaksAndRobes,
@@ -230,25 +230,25 @@ namespace Server.Engines.VeteranRewards
 					#endregion
 				};
 
-			const int Bronze = 0x972;
-			const int Copper = 0x96D;
-			const int Golden = 0x8A5;
-			const int Agapite = 0x979;
-			const int Verite = 0x89F;
-			const int Valorite = 0x8AB;
-			const int IceGreen = 0x47F;
-			const int IceBlue = 0x482;
-			const int DarkGray = 0x497;
-			const int Fire = 0x489;
-			const int IceWhite = 0x47E;
-			const int JetBlack = 0x001;
-			
-			#region Veteran Rewards
-			const int Pink		= 0x490;
-			const int Crimson	= 0x485;
-			#endregion
+            const int Bronze = 0x972;
+            const int Copper = 0x96D;
+            const int Golden = 0x8A5;
+            const int Agapite = 0x979;
+            const int Verite = 0x89F;
+            const int Valorite = 0x8AB;
+            const int IceGreen = 0x47F;
+            const int IceBlue = 0x482;
+            const int DarkGray = 0x497;
+            const int Fire = 0x489;
+            const int IceWhite = 0x47E;
+            const int JetBlack = 0x001;
 
-			m_Lists = new RewardList[]
+            #region Veteran Rewards
+            const int Pink = 0x490;
+            const int Crimson = 0x485;
+            #endregion
+
+            m_Lists = new RewardList[]
 				{
 					new RewardList( RewardInterval, 1, new RewardEntry[]
 					{
@@ -423,7 +423,7 @@ namespace Server.Engines.VeteranRewards
 					{
 						#region Veteran Rewards
 						new RewardEntry( houseAddOns,	1076157, typeof( CannonDeed ) ),					
-						new RewardEntry( houseAddOns,	1080550, typeof( TreeStumpDeed ) ),
+						new RewardEntry( houseAddOns,	1080550, typeof( TreeStumpDeed ) )
 						#endregion
 					} ),
 					new RewardList( RewardInterval, 8, new RewardEntry[]
@@ -440,18 +440,18 @@ namespace Server.Engines.VeteranRewards
 						#endregion
 					} ),
 					new RewardList( RewardInterval, 10, new RewardEntry[]
-					{												
+					{
 						#region Veteran Rewards
 						new RewardEntry( monsterStatues,	1080520, typeof( MonsterStatuette ), MonsterStatuetteType.Harrower ),
 						new RewardEntry( monsterStatues,	1080521, typeof( MonsterStatuette ), MonsterStatuetteType.Efreet ),
 
 						new RewardEntry( cloaksAndRobes,	1080382, typeof( RewardCloak ), Pink, 1080382 ),
-						new RewardEntry( cloaksAndRobes,	1080380, typeof( RewardRobe ), Pink, 1080380 ),						
+						new RewardEntry( cloaksAndRobes,	1080380, typeof( RewardRobe ), Pink, 1080380 ),
 						new RewardEntry( cloaksAndRobes,	1080378, typeof( RewardDress ), Pink, 1080378 ),
 						new RewardEntry( cloaksAndRobes,	1080383, typeof( RewardCloak ), Crimson, 1080383 ),
-						new RewardEntry( cloaksAndRobes,	1080381, typeof( RewardRobe ), Crimson, 1080381 ),						
+						new RewardEntry( cloaksAndRobes,	1080381, typeof( RewardRobe ), Crimson, 1080381 ),
 						new RewardEntry( cloaksAndRobes,	1080379, typeof( RewardDress ), Crimson, 1080379 ),
-						
+
 						new RewardEntry( etherealSteeds,	1080386, typeof( EtherealCuSidhe ) ),
 
 						new RewardEntry( houseAddOns,		1080548, typeof( MiningCartDeed ) ),
@@ -459,43 +459,43 @@ namespace Server.Engines.VeteranRewards
 						#endregion
 					} )
 				};
-		}
+        }
 
-		public static void Initialize()
-		{
-			if ( Enabled )
-				EventSink.Login += new LoginEventHandler( EventSink_Login );
-		}
+        public static void Initialize()
+        {
+            if (Enabled)
+                EventSink.Login += new LoginEventHandler(EventSink_Login);
+        }
 
-		private static void EventSink_Login( LoginEventArgs e )
-		{
-			if ( !e.Mobile.Alive )
-				return;
+        private static void EventSink_Login(LoginEventArgs e)
+        {
+            if (!e.Mobile.Alive)
+                return;
 
-			int cur, max, level;
+            int cur, max, level;
 
-			ComputeRewardInfo( e.Mobile, out cur, out max, out level );
+            ComputeRewardInfo(e.Mobile, out cur, out max, out level);
 
-			if ( e.Mobile.SkillsCap == 7000 || e.Mobile.SkillsCap == 7050 || e.Mobile.SkillsCap == 7100 || e.Mobile.SkillsCap == 7150 || e.Mobile.SkillsCap == 7200 )
-			{
-				if ( level > 4 )
-					level = 4;
-				else if ( level < 0 )
-					level = 0;
+            if (e.Mobile.SkillsCap == 7000 || e.Mobile.SkillsCap == 7050 || e.Mobile.SkillsCap == 7100 || e.Mobile.SkillsCap == 7150 || e.Mobile.SkillsCap == 7200)
+            {
+                if (level > 4)
+                    level = 4;
+                else if (level < 0)
+                    level = 0;
 
-				if ( SkillCapRewards )
-					e.Mobile.SkillsCap = 7000 + (level * 50);
-				else
-					e.Mobile.SkillsCap = 7000;
-			}
+                if (SkillCapRewards)
+                    e.Mobile.SkillsCap = 7000 + (level * 50);
+                else
+                    e.Mobile.SkillsCap = 7000;
+            }
 
-			if ( cur < max )
-				e.Mobile.SendGump( new RewardNoticeGump( e.Mobile ) );
-		}
-	}
+            if (cur < max)
+                e.Mobile.SendGump(new RewardNoticeGump(e.Mobile));
+        }
+    }
 
-	public interface IRewardItem
-	{
-		bool IsRewardItem{ get; set; }
-	}
+    public interface IRewardItem
+    {
+        bool IsRewardItem { get; set; }
+    }
 }
