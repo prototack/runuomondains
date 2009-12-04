8db939728c7b6c5924407f132fee4396011478cd
ShaiTanMalkier <ShaiTanMalkier@ab47cf70-d4a0-11dd-8179-53c65a4d6d95>
2009-12-04 04:52:15 +0000

    Fixed stealth problem with monsters ability to Stealth.

--- trunk/Scripts/Skills/Stealth.cs
+++ trunk/Scripts/Skills/Stealth.cs
@@ -88,8 +88,12 @@ namespace Server.SkillHandlers
 						steps = 1;
 
 					m.AllowedStealthSteps = steps;
-					PlayerMobile pm = m as PlayerMobile; // IsStealthing should be moved to Server.Mobiles
-					pm.IsStealthing = true;
+
+                    if (m is PlayerMobile)
+                    {
+                        PlayerMobile pm = m as PlayerMobile; // IsStealthing should be moved to Server.Mobiles
+                        pm.IsStealthing = true;
+                    }
 
 					m.SendLocalizedMessage( 502730 ); // You begin to move quietly.
 
